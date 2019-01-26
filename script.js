ad03f3ecd214a3532206838f3c7186d53a47655f
Aqib-Ul-Hassan <aqiib634@gmail.com>
2019-01-26 02:00:00 +0000

    webst

---
+++ script.js
@@ -0,0 +1,12 @@
+// Change colour of button when clicked
+
+function btn1(){
+  document.getElementById('mainButton').style.backgroundColor='#fff'; // Button colour
+  document.getElementById('mainButton').style.color='#000'; // Text colour
+
+}
+
+function btn2(){
+  document.getElementById('mainButton2').style.backgroundColor='#fff'; // Button colour
+  document.getElementById('mainButton2').style.color='#000'; // Text colour
+}
